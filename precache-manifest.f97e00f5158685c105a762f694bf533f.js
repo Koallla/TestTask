ce472adb36dd5da6a8326ccd811a8ad1e824553a
Koallla <zmiyov2422@gmail.com>
2020-01-12 23:22:29 +0200

    Updates

---
+++ precache-manifest.f97e00f5158685c105a762f694bf533f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2595bc3f63c56c05a461",
+    "url": "/TestTask/static/css/main.57525372.chunk.css"
+  },
+  {
+    "revision": "2595bc3f63c56c05a461",
+    "url": "/TestTask/static/js/main.f1eab4db.chunk.js"
+  },
+  {
+    "revision": "8bcde1b69c8ed2a30a9e",
+    "url": "/TestTask/static/js/runtime~main.87f0178b.js"
+  },
+  {
+    "revision": "f70f4b51112dc9f8c4a0",
+    "url": "/TestTask/static/css/2.a7d1321f.chunk.css"
+  },
+  {
+    "revision": "f70f4b51112dc9f8c4a0",
+    "url": "/TestTask/static/js/2.4fb68973.chunk.js"
+  },
+  {
+    "revision": "4b00020374127edfa8a346e26b0df5d2",
+    "url": "/TestTask/index.html"
+  }
+];
